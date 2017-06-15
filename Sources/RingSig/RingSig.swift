5effb8c32b6826116bbf97e8aa5a0908b23654e9
Fabio Tacke <tackefab@informatik.hu-berlin.de>
2017-06-15 18:05:58 +0200

    Add hashing and encryption/decryption parts of the signing algorithm

--- Sources/RingSig/RingSig.swift
+++ Sources/RingSig/RingSig.swift
@@ -8,13 +8,32 @@
 
 import Foundation
 import BigInt
+import CryptoSwift
 
 class RingSig {
   static func ringSign(message: BigUInt, nonSigneesPublicKeys: [RSA.PublicKey], signeeKeyPair: RSA.KeyPair) -> Signature {
-    // Choose a moduli for all the calculations that is sufficiently great
+    // 0. Choose a moduli for all the calculations that is sufficiently great
     let b = commonB(publicKeys: nonSigneesPublicKeys + [signeeKeyPair.publicKey])
+    print("b=\(b.description)")
+    
+    // 1. Compute the key as k = h(m)
+    let k = calculateDigest(message: message)
+    print("Length of k: \(k.width)")
+    
+    // 2. Pick a random glue value
+    let glue = BigUInt.randomInteger(withExactWidth: b.width)
+    
+    // 3.
     
     return Signature(publicKeys: [], glue: BigUInt(), xValues: [])
+  }
+  
+  /// Calculates the SHA256 hash digest of the given input.
+  ///
+  /// - Parameter message: The message whose hash digest is going to be computed
+  /// - Returns: SHA256 hash digest of the given message
+  internal static func calculateDigest(message: BigUInt) -> BigUInt {
+    return BigUInt(message.serialize().sha256())
   }
   
   /// Because all the usual RSA signature calculations use different moduli `n_i`, we need to choose a modulus b that is greater than the greatest modulus n_i.
@@ -23,7 +42,7 @@ class RingSig {
   ///
   /// - Parameter publicKeys: The public keys that specify the individual moduli
   /// - Returns: A modulus that is greater than the greatest `n_i` and a multiple of the AES block size
-  private static func commonB(publicKeys: [RSA.PublicKey]) -> BigUInt {
+  internal static func commonB(publicKeys: [RSA.PublicKey]) -> BigUInt {
     let nMax = publicKeys.reduce(1) { (result, publicKey) in
       return publicKey.n > result ? publicKey.n : result
     }
@@ -32,6 +51,50 @@ class RingSig {
       sufficientBits += 128 - (sufficientBits % 128)
     }
     return sufficientBits
+  }
+  
+  /// Computes the extended trap-door permutation `g_i` as described in the paper.
+  ///
+  /// - Parameters:
+  ///   - x: Input argument
+  ///   - publicKey: The public key holds the modulus needed for the calculation
+  ///   - modulus: The modulus `b` is the common modulus used for all the calculations
+  /// - Returns: `g(x) = qn+f(r)` where `x = qn+r` and `f(r)` is the RSA encryption operation `r^e`
+  internal static func g(x: BigUInt, publicKey: RSA.PublicKey, modulus: BigUInt) -> BigUInt {
+    let q = x / publicKey.n
+    var result = x
+    if (q + 1) * publicKey.n <= BigUInt(2).power(modulus.width) {
+      let r = x - (q * publicKey.n)
+      let fr = r.power(RSA.PublicKey.e, modulus: publicKey.n)
+      result = (q * publicKey.n) + fr
+    }
+    return result
+  }
+  
+  /// Encrypts the given message under the specified key using AES-256 CBC.
+  ///
+  /// - Requires: Message length must be a multiple of the block size of 128 bit.
+  /// - Parameters:
+  ///   - message: The message to be encrypted.
+  ///   - key: The (symmetric) key.
+  /// - Returns: Ciphertext of AES-256 CBC encrypted message.
+  internal static func encrypt(message: BigUInt, key: BigUInt) -> BigUInt {
+    precondition(message.width % 128 == 0)
+    let aes = try! AES(key: key.serialize().bytes, iv: ">RingSiggiSgniR<".data(using: .utf8)!.bytes, blockMode: .CBC, padding: NoPadding())
+    let ciphertext = try! aes.encrypt(message.serialize().bytes)
+    return BigUInt(Data(bytes: ciphertext))
+  }
+  
+  /// Decrypts the given ciphertext under the specified key using AES-256 CBC.
+  ///
+  /// - Parameters:
+  ///   - cipher: The ciphertext to be decrypted.
+  ///   - key: The (symmetric) key.
+  /// - Returns: Plaintext of AES-256 CBC decrypted ciphertext.
+  internal static func decrypt(cipher: BigUInt, key: BigUInt) -> BigUInt {
+    let aes = try! AES(key: key.serialize().bytes, iv: ">RingSiggiSgniR<".data(using: .utf8)!.bytes, blockMode: .CBC, padding: NoPadding())
+    let plaintext = try! aes.decrypt(cipher.serialize().bytes)
+    return BigUInt(Data(bytes: plaintext))
   }
   
   struct Signature {
--- Tests/RingSigTests/RingSigTests.swift
+++ Tests/RingSigTests/RingSigTests.swift
@@ -3,10 +3,41 @@ import BigInt
 @testable import RingSig
 
 class RingSigTests: XCTestCase {
-    func testRSA() {
-      let keyPair = RSA.generateKeyPair(length: 64)
-      let message = BigUInt("Hello, World!".data(using: .utf8)!)
-      let signature = RSA.sign(message: message, privateKey: keyPair.privateKey, publicKey: keyPair.publicKey)
-      XCTAssert(RSA.verify(message: message, signature: signature, publicKey: keyPair.publicKey))
-    }
+  func testRSA() {
+    let keyPair = RSA.generateKeyPair(length: 64)
+    let message = BigUInt("Hello, World!".data(using: .utf8)!)
+    let signature = RSA.sign(message: message, privateKey: keyPair.privateKey, publicKey: keyPair.publicKey)
+    XCTAssert(RSA.verify(message: message, signature: signature, publicKey: keyPair.publicKey))
+  }
+  
+  func testModulusCalculation() {
+    var publicKeys = [RSA.PublicKey(n: BigUInt(234)), RSA.PublicKey(n: BigUInt(567)), RSA.PublicKey(n: BigUInt(123))]
+    XCTAssertEqual(RingSig.commonB(publicKeys: publicKeys), BigUInt(256))
+    
+    publicKeys.append(RSA.PublicKey(n: BigUInt(2).power(96) - 1))
+    XCTAssertEqual(RingSig.commonB(publicKeys: publicKeys), BigUInt(256))
+  }
+  
+  func testHashing() {
+    let message = BigUInt("Hello, World!".data(using: .utf8)!)
+    let digest = RingSig.calculateDigest(message: message)
+    XCTAssertEqual(digest.serialize().toHexString(), "dffd6021bb2bd5b0af676290809ec3a53191dd81c7f70a4b28688a362182986f")
+  }
+  
+  func testG() {
+    let publicKey = RSA.PublicKey(n: 35)
+    let y = RingSig.g(x: 75, publicKey: publicKey, modulus: 128)
+    XCTAssertEqual(y, 80)
+  }
+  
+  func testEncryption() {
+    let key = BigUInt(2).power(256) - 1
+    XCTAssertEqual(key.width, 256)
+    let message = BigUInt(2).power(128) - 1
+    XCTAssertEqual(message.width, 128)
+    
+    let cipher = RingSig.encrypt(message: message, key: key)
+    let plaintext = RingSig.decrypt(cipher: cipher, key: key)
+    XCTAssertEqual(plaintext, message)
+  }
 }
